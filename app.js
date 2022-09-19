722e3132b33084ac3952d8b9beb601e5af478e62
Anton Viljanen <Anton Viljanen@sal.local>
2022-09-19 15:48:40 +0300

    second comit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const _ = require("lodash");
+const { readFile } = require("fs");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+console.log("started a first task");
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("completed first task");
+});
+console.log("starting next task");
